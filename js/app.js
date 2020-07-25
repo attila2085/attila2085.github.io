c528571f68105ba4d1903c6ad495b98597b4ca0b
unknown <attilafekete85@gmail.com>
2020-07-25 14:08:52 +0300

    Init page.

--- js/app.js
+++ js/app.js
@@ -12,3 +12,16 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 3000); // Change image every 2 seconds
 }
+
+
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+  console.log(x);
+}
