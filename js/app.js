101c0296f4029df02c9fd38a524a428fcb883f55
attila2085 <attilafekete85@gmail.com>
2021-03-27 13:03:30 +0200

    re

--- js/app.js
+++
@@ -1,8 +0,0 @@
-const img = document.getElementById("product-image");
-console.log(img);
-const imgdiv = document.getElementById("cards");
-console.log(imgdiv);
-img.addEventListener(onmouseover)=>
-{
-
-}
